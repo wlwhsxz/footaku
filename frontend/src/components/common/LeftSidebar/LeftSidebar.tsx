d5687220ed3a68601d48f50d34832f77c5600613
minu <wlwhsxz@gmail.com>
2024-02-15 11:54:36 +0900

    feat: add LeftSidebar routing

--- frontend/src/components/common/LeftSidebar/LeftSidebar.tsx
+++ frontend/src/components/common/LeftSidebar/LeftSidebar.tsx
@@ -1,26 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { leftSidebarLinks, Nav } from '../../../constatns/leftSidebarLinks';
+import { Nav, leftSidebarLinks } from '../../../constatns/leftSidebarLinks';
 
 const LeftSidebar: React.FC = () => {
-  console.log(leftSidebarLinks)
+
+  const NavHandler = (label: string) => (e: React.MouseEvent) => {
+    console.log(`Clicked on: ${label}`);
+    // 여기에 필요한 로직을 추가할 수 있습니다. 예를 들어, 페이지 이동 또는 상태 업데이트 등
+  }
+
   return (
     <Wrapper>
       <LogoBox>
         <h2>Footaku</h2>
       </LogoBox>
         {leftSidebarLinks.map((nav: Nav) => (
-          <NavBox key={nav.label}>
-            <NavIcon>
-              <nav.icon />
-            </NavIcon>
-            <NavLabel>
-              {nav.label}
-            </NavLabel>
-          </NavBox>
+          <Link to={nav.route} key={nav.label}>
+            <NavBox>
+              <NavIcon>
+                <nav.icon />
+              </NavIcon>
+              <NavLabel>
+                {nav.label}
+              </NavLabel>
+            </NavBox>
+          </Link>
         ))}
     </Wrapper>
   );
 };
+
 
 export default LeftSidebar;
 
@@ -55,6 +65,8 @@ const NavBox = styled.div`
     cursor: pointer;
     background: rgb(255,255,255,0.2)
   }
+
+  color: white;
 `
 
 const NavIcon = styled.div`
