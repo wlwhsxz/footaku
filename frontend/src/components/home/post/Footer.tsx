874e14e18d5054d06faa6dc48c536c3c02be2f04
minu <wlwhsxz@gmail.com>
2024-02-15 11:26:51 +0900

    feat: homepage_post_footer

--- frontend/src/components/home/post/Footer.tsx
+++ frontend/src/components/home/post/Footer.tsx
@@ -22,7 +22,18 @@ const Footer = () => {
           </div>
         </RightButtonBox>
       </ButtonSection>
-      <TextSection>content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text </TextSection>
+      <TextSection>
+        <LikeText>
+          3,654,321 likes
+        </LikeText>
+        <TitleText>       
+          content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text 
+        </TitleText>
+        <CommentText>
+          view all 4,219 comments
+          <input placeholder="Add a comment..."/>
+        </CommentText>
+      </TextSection>
     </FooterContainer>
   )
 }
@@ -33,9 +44,8 @@ const FooterContainer = styled.div`
   display: flex;
   flex-direction: column;
 
-  div {
-    padding: 8px;
-  }
+
+  border-bottom: 1px solid gray;
 
   img {
     width: 24px;
@@ -51,19 +61,54 @@ const ButtonSection = styled.div`
   justify-content: space-between;
 
   width: 100%;
+
 `
 
 const LeftButtonBox = styled.div`
   display: flex;
+
+  div {
+    padding: 8px;
+  }
 `
 
-const RightButtonBox = styled.div``
+const RightButtonBox = styled.div`
+div {
+  padding: 8px;
+}
+`
 
 const TextSection = styled.div`
   text-align: left;
+`;
+
+const LikeText = styled.div`
+
+`
+
+const TitleText = styled.div`
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
-`;
+`
+
+const CommentText = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  color: grey;
+
+  input {
+    padding: 5px 0px;
+    text-align: left;
+    border: none;
+    background-color: transparent;
+    
+    &:focus {
+      outline: none;
+    }
+  }
+  
+`
