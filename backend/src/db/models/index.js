339bc7d6bf06b73348d8a5a0903d36b161a1a360
minu <wlwhsxz@gmail.com>
2024-02-18 20:43:14 +0900

    feat: create post schema

--- backend/src/db/models/index.js
+++ backend/src/db/models/index.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
 const UserSchema = require("../schemas/userSchema");
+const PostSchema = require("../schemas/postSchema");
 
 const User = mongoose.model("User", UserSchema);
+const Post = mongoose.model("Post", PostSchema);
 
 module.exports = {
   User,
+  Post,
 };
