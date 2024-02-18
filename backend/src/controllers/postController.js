0ea7190ad9267fd359cc419a7350071345d69c29
minu <wlwhsxz@gmail.com>
2024-02-18 23:57:10 +0900

    fix: fixed wrong status code 201 -> 200

--- backend/src/controllers/postController.js
+++ backend/src/controllers/postController.js
@@ -6,18 +6,12 @@ const {
 
 //[ 포스트 전체 요청 ]
 const getAllPosts = async (req, res, next) => {
-  if (error) {
-    const message = errorMessageHandler(error);
-    return next(new AppError(400, message));
-  }
-
   try {
     const { statusCode, message, data } = await postService.getAllPosts();
-    console.log(data);
 
-    if (statusCode !== 201) return next(new AppError(statusCode, message));
+    if (statusCode !== 200) return next(new AppError(statusCode, message));
 
-    res.status(201).json({ message, data });
+    res.status(200).json({ message, data });
   } catch (error) {
     console.error(error);
     return next(new AppError(500, "Internal Server Error"));
