e9901b081f0863cea7aebeb270fa7eaf63a7798c
minu <wlwhsxz@gmail.com>
2024-02-27 11:31:24 +0900

    feat: add getPostById API

--- backend/src/controllers/postController.js
+++ backend/src/controllers/postController.js
@@ -18,6 +18,21 @@ const getAllPosts = async (req, res, next) => {
   }
 };
 
+const getPostById = async (req, res, next) => {
+  try {
+    const { postId } = req.params;
+    const { statusCode, message, data } = await postService.getPostById(postId);
+
+    if (statusCode !== 200) return next(new AppError(statusCode, message));
+
+    res.status(200).json({ message, data });
+  } catch (error) {
+    console.error(error);
+    return next(new AppError(500, "Internal Server Error"));
+  }
+};
+
 module.exports = {
   getAllPosts,
+  getPostById,
 };
--- backend/src/routes/postRouter.js
+++ backend/src/routes/postRouter.js
@@ -6,4 +6,6 @@ const postController = require("../controllers/postController");
 //[ 포스트 전체 요청 ]
 router.get("/", postController.getAllPosts);
 
+router.get("/:postId", postController.getPostById);
+
 module.exports = router;
