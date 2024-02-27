a665511a0ee5904af286a934061e49e48da86173
minu <wlwhsxz@gmail.com>
2024-02-28 01:01:05 +0900

    chore: change Filename

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,24 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Home from './pages/homePage/Home';
-import Login from './pages/userPage/Login';
-import Signup from './pages/userPage/Sign-up';
-import News from './pages/newsPage/News';
-import Instagram from './pages/instagramPage/Instagram';
-import Likes from './pages/likesPage/Likes';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/homePage/Home";
+import Login from "./pages/userPage/Login";
+import SignUp from "./pages/userPage/SignUp";
+import News from "./pages/newsPage/News";
+import Instagram from "./pages/instagramPage/Instagram";
+import Likes from "./pages/likesPage/Likes";
 
 const App = () => {
   return (
-    <Router >
+    <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />}/>
-        <Route path='/sign-up' element={<Signup />} />
-        <Route path='/news' element={<News />} />
-        <Route path='/instagram' element={<Instagram />} />
-        <Route path='/me/likes' element={<Likes />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/instagram" element={<Instagram />} />
+        <Route path="/me/likes" element={<Likes />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
