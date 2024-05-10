9b8fd3c2064ee6f9dbe4afe492f39d7b2b8fbdbb
minu <wlwhsxz@gmail.com>
2024-05-10 23:54:47 +0900

    feat: make seperate Close Button component

---
+++ frontend/src/components/common/buttons/CloseButton.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+interface CloseButtonProps {
+  onClose: () => void;
+}
+
+const CloseButton: React.FC<CloseButtonProps> = ({ onClose }) => {
+  return (
+    <StyledCloseButton onClick={onClose}>
+      <img
+        src="https://cdn-icons-png.flaticon.com/128/458/458595.png"
+        alt="Close"
+      />
+    </StyledCloseButton>
+  );
+};
+
+const StyledCloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  padding: 20px 20px 0 0;
+
+  img {
+    width: 16px;
+    height: 16px;
+  }
+`;
+
+export default CloseButton;
