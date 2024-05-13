477ac4ea120b94e5e5f42888aac921325d59ad26
minu <wlwhsxz@gmail.com>
2024-05-13 23:47:10 +0900

    feat: add show more options function in MoreButton component

--- frontend/src/components/common/buttons/MoreButton.tsx
+++ frontend/src/components/common/buttons/MoreButton.tsx
@@ -1,24 +1,48 @@
-import React, { useState } from "react";
+import React from "react";
+import DeleteOption from "../moreOptions/DeleteOption";
 
 interface MoreButtonProps {
   className?: string;
+  isActive?: boolean;
+  onClick?: () => void;
+  buttonId?: string;
+  onClose: () => void;
 }
 
-const MoreButton: React.FC<MoreButtonProps> = ({ className, ...props }) => {
-  const [clicked, setClicked] = useState(false);
+const MoreButton: React.FC<MoreButtonProps> = ({
+  className,
+  isActive,
+  onClick,
+  buttonId,
+  onClose,
+  ...props
+}) => {
+  const renderOptions = () => {
+    switch (buttonId) {
+      case "delete":
+        return <DeleteOption onClose={handleClose} />;
+      case "":
+        return null;
+      default:
+        return null;
+    }
+  };
 
-  const handleClick = () => setClicked(!clicked);
+  const handleClose = () => {
+    buttonId = "";
+  };
 
   return (
     <span className={className} {...props}>
       <img
         src="https://cdn4.iconfinder.com/data/icons/pictype-free-vector-icons/16/more-512.png"
-        onClick={handleClick}
+        onClick={onClick}
         role="button"
         tabIndex={0}
         style={{ cursor: "pointer" }}
         alt="more_button"
       />
+      {isActive && renderOptions()}
     </span>
   );
 };
