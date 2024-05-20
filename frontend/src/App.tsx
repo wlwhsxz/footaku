bd6a16529baac6f874e4c02fbee3b6b0c6087836
minu <wlwhsxz@gmail.com>
2024-05-20 23:59:27 +0900

    feat: add new page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Login from "./pages/userPage/Login";
 import SignUp from "./pages/userPage/SignUp";
 import Club from "./pages/clubPage/Club";
 import useAuthStore from "./store/useAuthStore";
+import ClubNews from "./pages/clubPage/ClubNews";
 
 const App = () => {
   const isLoggedIn = useAuthStore((state) => !!state.user);
@@ -16,6 +17,10 @@ const App = () => {
         <Route
           path="/club/:clubName"
           element={isLoggedIn ? <Club /> : <Login />}
+        />
+        <Route
+          path="/club/:clubName/news"
+          element={isLoggedIn ? <ClubNews /> : <Login />}
         />
       </Routes>
     </Router>
