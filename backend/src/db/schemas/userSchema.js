25f41e5b502baf678b0c46586b57cf5402248a9f
minu <wlwhsxz@gmail.com>
2024-05-23 22:59:57 +0900

    feat: add profileImg field

--- backend/src/db/schemas/userSchema.js
+++ backend/src/db/schemas/userSchema.js
@@ -18,6 +18,10 @@ const UserSchema = new Schema(
       type: String,
       required: true,
     },
+    profileImg: {
+      type: String,
+      default: "",
+    },
     email: {
       type: String,
       required: true,
