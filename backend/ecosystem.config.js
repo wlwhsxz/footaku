92aa73bf7db08b5f5f0e983d3ca53b14322f70e3
root <root@ip-172-31-4-239.ap-northeast-2.compute.internal>
2024-05-24 07:21:05 +0000

    modify: seperate backend npm run dev & start options

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  apps : [{
+    name: 'footaku-dev',	  
+    script: 'cd src && nodemon index.js',
+    instances: 1,
+    watch: 'false',
+    env: {
+        NODE_ENV: 'development',
+    },
+  }, {
+    name: 'footaku-production',
+    script: 'node src/index.js',
+    instances: -1,
+    autorestart: false,
+    watch: false,
+    env_production: {
+        NODE_ENV: 'production',
+        PORT: 8080,
+    },
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
