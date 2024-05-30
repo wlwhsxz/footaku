1b0b5a5a91ce866b76760519ad6bd40f6513e915
minu <wlwhsxz@gmail.com>
2024-05-30 23:18:55 +0900

    modify: seperate moreButton handler component from PostDetail

---
+++ frontend/src/components/common/moreOptions/moreButtonHandler.ts
@@ -0,0 +1,24 @@
+export const handleMoreButtonClick = (
+  buttonId: string,
+  setPopupContent: React.Dispatch<React.SetStateAction<string[]>>,
+  setIsPopupOpen: React.Dispatch<React.SetStateAction<boolean>>,
+  setCurrentCommentId?: React.Dispatch<React.SetStateAction<string | null>>,
+  commentId?: string
+) => {
+  switch (buttonId) {
+    case "comment":
+      setPopupContent(["Delete", "Cancel"]);
+      if (setCurrentCommentId && commentId) {
+        setCurrentCommentId(commentId);
+        console.log(commentId);
+      }
+
+      break;
+    case "post":
+      setPopupContent(["Share", "Report", "Cancel"]);
+      break;
+    default:
+      setPopupContent([]);
+  }
+  setIsPopupOpen(true);
+};
