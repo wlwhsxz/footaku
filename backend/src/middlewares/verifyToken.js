63337b7c04fa7dc75c0b61d8dabf95ee77f19e23
minu <wlwhsxz@gmail.com>
2024-06-05 19:18:01 +0900

    fix: fix wrong like/unlike logic

--- backend/src/middlewares/verifyToken.js
+++ backend/src/middlewares/verifyToken.js
@@ -13,37 +13,50 @@ const verifyToken = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(accessToken, env.ACCESS_TOKEN_SECRET);
+    console.log("Access token verified");
 
     req.user = decoded;
     next();
   } catch (error) {
     if (error.name === "TokenExpiredError" && refreshToken) {
+      console.log("Access token expired, verifying refresh token...");
+
       try {
-        const decoded = jwt.verify(refreshToken, env.REFRESH_TOKEN_SECRET);
+        const decodedRefresh = jwt.verify(
+          refreshToken,
+          env.REFRESH_TOKEN_SECRET
+        );
+        console.log("Refresh token verified:", decodedRefresh);
+
         const newAccessToken = jwt.sign(
           {
-            userId: decoded.userId,
-            userName: decoded.userName,
-            email: decoded.email,
+            userId: decodedRefresh.userId,
+            userName: decodedRefresh.userName,
+            email: decodedRefresh.email,
           },
           env.ACCESS_TOKEN_SECRET,
           {
             expiresIn: env.ACCESS_TOKEN_EXPIRES_IN,
           }
         );
+
         res.cookie("accessToken", newAccessToken, {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
           sameSite: "strict",
         });
+        console.log("New access token issued:", newAccessToken);
+
         req.user = jwt.decode(newAccessToken);
         next();
-      } catch (error) {
+      } catch (refreshError) {
+        console.error("Refresh token verification failed:", refreshError);
         return res
           .status(401)
           .json({ message: "리프레시 토큰이 유효하지 않습니다." });
       }
     } else {
+      console.error("Access token verification failed:", error);
       return res.status(401).json({ message: "토큰이 유효하지 않습니다." });
     }
   }
